ca8e64329a91e2f545657f5c66bc189b187cc46d
Fredrik Larsson <nossralf@gmail.com>
2016-05-17 13:45:39 +0200

    Move package archive setup to custom.el

--- init.el
+++ init.el
@@ -1,10 +1,7 @@
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
 (require 'package)
-
-(add-to-list 'package-archives
-    '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives
-    '("melpa" . "https://melpa.org/packages/") t)
-
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
