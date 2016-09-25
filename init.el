5d9e759cb486d85c53e981960ca18bb475ace1c0
Fredrik Larsson <nossralf@gmail.com>
2016-09-25 18:58:43 +0200

    Use flycheck-rust when rust-mode is active

--- init.el
+++ init.el
@@ -207,6 +207,7 @@
   :mode "\\.rs\\'"
   :config
   (setq racer-rust-src-path "~/.rust-source/stable/src")
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'rust-mode-hook #'cargo-minor-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
