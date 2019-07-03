7dc292e6dad256e286732ad18d2d2edf12837013
Fredrik Larsson <nossralf@gmail.com>
2019-07-03 10:54:12 +0200

    Add org-mode package index

--- custom.el
+++ custom.el
@@ -27,7 +27,8 @@
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")
-     ("melpa" . "https://melpa.org/packages/"))))
+     ("melpa" . "https://melpa.org/packages/")
+     ("org" . "https://orgmode.org/elpa/"))))
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
