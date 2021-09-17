f2ae1e53346eff597803ae918bff149f792cab1b
Fredrik Larsson <nossralf@gmail.com>
2021-09-17 23:06:07 +0200

    Clean up custom file

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))
  '(display-line-numbers-grow-only t)
  '(display-line-numbers-width-start t)
  '(exec-path-from-shell-variables '("PATH" "MANPATH" "PKG_CONFIG_PATH"))
@@ -27,24 +26,23 @@
  '(markdown-command
    "pandoc --from commonmark --to html5 --highlight-style pygments --standalone")
  '(neo-smart-open t)
- '(neo-theme (quote nerd))
+ '(neo-theme 'nerd)
  '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
+   '(("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")
-     ("melpa" . "https://melpa.org/packages/"))))
+     ("melpa" . "https://melpa.org/packages/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
     (lsp-mode aggressive-indent anzu better-defaults cargo company dockerfile-mode erlang exec-path-from-shell expand-region fish-mode flycheck flycheck-color-mode-line flycheck-rust forge git-commit groovy-mode hardcore-mode helm helm-flycheck helm-projectile helm-rg highlight-symbol highlight-symbol magit markdown-mode move-text neotree paredit popwin projectile puppet-mode racer rainbow-delimiters rjsx-mode rust-mode smart-mode-line toml-mode undo-tree use-package which-key yaml-mode yasnippet zenburn-theme)))
- '(projectile-completion-system (quote helm))
- '(python-fill-docstring-style (quote symmetric))
+ '(projectile-completion-system 'helm)
+ '(python-fill-docstring-style 'symmetric)
  '(require-final-newline t)
  '(sentence-end-double-space nil)
  '(sml/theme 'respectful)
  '(too-hardcore-backspace t)
  '(too-hardcore-return t)
- '(whitespace-style (quote (face empty lines-tail trailing))))
+ '(whitespace-style '(face empty lines-tail trailing)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
