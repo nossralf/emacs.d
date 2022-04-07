f1198cca69aa57ef698310bc6c0adbfa0d77c09d
Fredrik Larsson <nossralf@gmail.com>
2022-04-07 22:05:48 +0200

    Call tool-/menu-/scroll-bar functions to disable stuff in early-init

--- early-init.el
+++ early-init.el
@@ -1,7 +1,9 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(setq tool-bar-mode nil
-      menu-bar-mode nil
-      scroll-bar-mode nil
-      package-enable-at-startup nil
+(horizontal-scroll-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq package-enable-at-startup nil
       frame-title-format nil)
