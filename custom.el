29d02cbe3b423e743ff21762c4e66ffdd04f9d63
Fredrik Larsson <nossralf@gmail.com>
2022-11-03 18:47:51 +0100

    Remove package-archives from custom file
    When using straight.el it's not needed.

--- custom.el
+++ custom.el
@@ -36,10 +36,6 @@
  '(neo-theme 'nerd)
  '(nxml-child-indent 4)
  '(org-src-fontify-natively t)
- '(package-archives
-   '(("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "https://stable.melpa.org/packages/")
-     ("melpa" . "https://melpa.org/packages/")))
  '(projectile-completion-system 'helm)
  '(python-fill-docstring-style 'symmetric)
  '(require-final-newline t)
