bcf5eae954d6b10da84b063214ad2b76052d54e4
Fredrik Larsson <git@fredriklarsson.dev>
2024-06-13 22:57:59 +0200

    Correctly capitalize JetBrains in font name

--- custom.el
+++ custom.el
@@ -55,4 +55,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Jetbrains Mono")))))
+ '(default ((t (:family "JetBrains Mono")))))
