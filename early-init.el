295445c2e04cfb432fbc10b60be60469183019bd
Fredrik Larsson <git@fredriklarsson.dev>
2024-06-23 22:38:43 +0200

    Disable scroll bars, menu bar, tool bar using custom file

--- early-init.el
+++ early-init.el
@@ -1,11 +1,5 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(horizontal-scroll-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-
 (setq package-enable-at-startup nil
       frame-title-format nil)
 
